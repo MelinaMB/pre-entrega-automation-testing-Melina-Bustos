cf05c4b96c634f257aecb52761704f1e0a453fb2
Melina Bustos <melina.m.bustos@gmail.com>
2025-10-24 12:35:59 -0300

    pre-entrega

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
 from utils import login
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    chrome_opt = Options() #abre chrome como incognito y desactiva popup de contraseña filtrada 
+    chrome_opt.add_argument("--incognito")
+    driver = webdriver.Chrome(options = chrome_opt)
     yield driver # se utiliza para los fixture en pytest para devolver un valor al test
                 # despues de que el test termina continua ejecutando el test que viene
     driver.quit()
--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ from selenium.webdriver.common.by import By
 
 def login(driver):
     
+    #voy a la pagina que quiero testear
     driver.get("https://www.saucedemo.com/")
 
+    #mando las credenciales para hacer el login
     driver.find_element(By.ID,"user-name").send_keys("standard_user")
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
     driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
